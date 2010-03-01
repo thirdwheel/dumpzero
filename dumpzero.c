887608fc7ec200f89a6e629af5a8f50b945fbc62
thirdwheel <absorbentshoulderman@gmail.com>
2010-03-02 02:50:55 +0300

    Added comments to dumpzero.c

--- dumpzero.c
+++ dumpzero.c
@@ -1,20 +1,58 @@
+/* Copyright (c) 2010, Aaron Mason
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ *  o Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *  o Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ *  o Neither the name of the <ORGANIZATION> nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
 #include <stdio.h>
 int main(void) {
+	/* Store a count of the null bytes found */
 	unsigned long count_null = 0;
+
+	/* Store the character read from stdin */
 	int byte;
+
+	/* Read successive characters from stdin until EOF */
 	while ((byte=getchar()) != EOF) {
+		/* Increment the number of nulls if we've found one */
 		if (byte == 0) {
 			count_null++;
 		} else {
-			if (count_null > 0)
+			/* If we've found nulls before, print them out */
+			if (count_null > 0) /* Print a debugging message */
 				fprintf(stderr, "writing %lu nulls\n", count_null);
 			while (count_null != 0) {
 				putchar(0);
 				count_null--;
 			}
+			/* Print the character to stdout */
 			putchar(byte);
 		}
 	}
-	
+	/* Operation complete, let's quit. */
 	return 0;
 }
